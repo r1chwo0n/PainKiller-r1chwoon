c9a5e7390692f9e905eb71b8ca4d3d73f9ae00aa
PoomNattapoom <rockyaggi@gmail.com>
2025-03-01 23:12:16 +0700

    format updated according to main

--- frontend/src/pages/addStockForm.tsx
+++ frontend/src/pages/addStockForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Sidebar from "../components/sidebar";
+import useSnackbar from "../components/useSnackber";
 
 const AddStockForm: React.FC = () => {
   const [formData, setFormData] = useState({
@@ -11,8 +12,7 @@ const AddStockForm: React.FC = () => {
     expired: "",
   });
 
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
-  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const { showSnackbar, Snackbar } = useSnackbar();
 
   const handleChange = (
     e: React.ChangeEvent<
@@ -34,8 +34,10 @@ const AddStockForm: React.FC = () => {
       !formData.amount ||
       !formData.expired
     ) {
-      setErrorMessage("กรุณากรอกข้อมูลให้ครบถ้วน");
-      setTimeout(() => setErrorMessage(null), 3000);
+      showSnackbar({
+        message: "กรุณากรอกข้อมูลให้ครบถ้วน",
+        severity: "error",
+      });
       return;
     }
 
@@ -59,17 +61,15 @@ const AddStockForm: React.FC = () => {
 
       // Send payload to the API
       /* Edit This */
-      const response = await axios.post(
-        "/api/stocks",
-        stockPayload,
-        {
-          headers: { "Content-Type": "application/json" },
-        }
-      );
+      const response = await axios.post("/api/stocks", stockPayload, {
+        headers: { "Content-Type": "application/json" },
+      });
 
       if (response.status === 201) {
-        setSuccessMessage("บันทึกข้อมูลสำเร็จ!");
-        setTimeout(() => setSuccessMessage(null), 3000);
+        showSnackbar({
+          message: "บันทึกข้อมูลยาสำเร็จ!",
+          severity: "success",
+        });
 
         // Reset form
         setFormData({
@@ -84,34 +84,30 @@ const AddStockForm: React.FC = () => {
       }
     } catch (error: any) {
       console.error("Error adding drug:", error);
-      setErrorMessage(
-        error.response?.data?.message || "เกิดข้อผิดพลาด โปรดลองอีกครั้ง"
-      );
-      setTimeout(() => setErrorMessage(null), 3000);
+      showSnackbar({
+        message:
+          error.response?.data?.message ||
+          "มีข้อผิดพลาดในการบันทึกข้อมูลยา โปรดตรวจสอบอีกครั้ง",
+        severity: "error",
+      });
     }
   };
 
   return (
     <div className="flex h-screen bg-[#f0f0f0]">
       <Sidebar />
-      <main className="flex-1 p-2">
-        <header className="bg-white h-[70px] p-4 rounded-[8px] shadow-md mb-4">
+      <main className="flex-1 p-4">
+        <header className="bg-white h-[86Spx] w-full p-6 rounded-[12px] shadow-md mb-6 flex items-center justify-between">
           <div className="flex justify-between items-center">
-            <h1 className="mb-2 text-4xl text-[#444444] font-bold ">
-              เพิ่มสต็อก
-            </h1>
+            <h1 className="text-4xl text-[#444444] font-bold ">เพิ่มสต็อก</h1>
           </div>
         </header>
 
-        <div className="bg-white shadow-md rounded-lg p-4 max-h-full overflow-hidden">
-          {errorMessage && (
-            <p className="text-red-500 text-center mb-2">{errorMessage}</p>
-          )}
-          {successMessage && (
-            <p className="text-green-500 text-center mb-2">{successMessage}</p>
-          )}
+        <div className="flex-1 bg-white rounded-[12px] pt-2 pr-4 pl-4 pb-5 overflow-y-sch">
+          {/* Error and success messages */}
+
           <form onSubmit={handleSubmit}>
-            <div className="grid grid-cols-2 gap-5 items-start">
+            <div className="grid grid-cols-2 gap-5 items-start mt-2">
               {/* Drug Name */}
               <div className="col-span-1">
                 <label htmlFor="name" className="text-[16px] text-[#444444]">
@@ -123,7 +119,7 @@ const AddStockForm: React.FC = () => {
                   name="name"
                   value={formData.name}
                   onChange={handleChange}
-                  className="w-full h-[40px] py-1 px-2 rounded-[8px] bg-[#f0f0f0] text-[#909090] focus:outline-none focus:ring-2 focus:ring-[#FB6F92]"
+                  className="w-full h-[40px] py-1 px-2 rounded-[8px] bg-[#f0f0f0] focus:outline-none focus:ring-2 focus:ring-[#FB6F92]"
                 />
               </div>
               {/* Unit */}
@@ -135,7 +131,7 @@ const AddStockForm: React.FC = () => {
                   name="unit_type"
                   value={formData.unit_type}
                   onChange={handleChange}
-                  className="w-full h-[40px] py-1 px-2 rounded-[8px] bg-[#f0f0f0] text-[#909090] focus:outline-none focus:ring-2 focus:ring-[#FB6F92]"
+                  className="w-full h-[40px] py-1 px-2 rounded-[8px] bg-[#f0f0f0] focus:outline-none focus:ring-2 focus:ring-[#FB6F92]"
                   required>
                   <option value="">เลือก</option>
                   <option value="กิโลกรัม">กิโลกรัม</option>
@@ -165,7 +161,7 @@ const AddStockForm: React.FC = () => {
                   }}
                   min="0"
                   step="1"
-                  className="w-full h-[40px] py-1 px-2 rounded-[8px] bg-[#f0f0f0] text-[#909090] focus:outline-none focus:ring-2 focus:ring-[#FB6F92]"
+                  className="w-full h-[40px] py-1 px-2 rounded-[8px] bg-[#f0f0f0]  focus:outline-none focus:ring-2 focus:ring-[#FB6F92]"
                 />
               </div>
 
@@ -180,7 +176,7 @@ const AddStockForm: React.FC = () => {
                   name="expired"
                   value={formData.expired}
                   onChange={handleChange}
-                  className="w-full h-[40px] py-1 px-2 rounded-[8px] bg-[#f0f0f0] text-[#909090] focus:outline-none focus:ring-2 focus:ring-[#FB6F92]"
+                  className="w-full h-[40px] py-1 px-2 rounded-[8px] bg-[#f0f0f0] focus:outline-none focus:ring-2 focus:ring-[#FB6F92]"
                 />
               </div>
 
@@ -205,7 +201,7 @@ const AddStockForm: React.FC = () => {
                   }}
                   min="0"
                   step="1"
-                  className="w-full h-[40px] py-1 px-2 rounded-[8px] bg-[#f0f0f0] text-[#909090] focus:outline-none focus:ring-2 focus:ring-[#FB6F92]"
+                  className="w-full h-[40px] py-1 px-2 rounded-[8px] bg-[#f0f0f0] focus:outline-none focus:ring-2 focus:ring-[#FB6F92]"
                 />
               </div>
             </div>
@@ -218,6 +214,7 @@ const AddStockForm: React.FC = () => {
                   บันทึก
                 </button>
               </div>
+              {Snackbar}
             </div>
           </form>
         </div>
