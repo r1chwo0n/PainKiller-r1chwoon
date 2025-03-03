b4c46c9e2747f59b335fc2db9ae023959a4625e8
KonLew <surangrat231@gmail.com>
2025-03-03 16:40:19 +0700

    final config for deploy(มั้ง)

--- backend/db/utils.ts
+++ backend/db/utils.ts
@@ -4,7 +4,7 @@ const dbUser = process.env.POSTGRES_APP_USER;
 const dbPassword = process.env.POSTGRES_APP_PASSWORD;
 const dbHost = process.env.POSTGRES_HOST;
 // const dbPort = process.env.POSTGRES_PORT || 5003; // Default to 5432 if not provided
-const dbPort = 5432;
+const dbPort = process.env.POSTGRES_PORT_IN;
 const dbName = process.env.POSTGRES_DB;
 
 if (!dbUser || !dbPassword || !dbHost || !dbPort || !dbName) {
